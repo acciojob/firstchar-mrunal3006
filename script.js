2daa5fc90b8acdeeccb78dec04cfc5cd58e1f118
mrunal3006 <128721188+mrunal3006@users.noreply.github.com>
2023-12-05 20:13:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstChar(text) {
   // your code here
+	 // Trim leading and trailing spaces
+  text = text.trim();
+
+  // Check if the string is empty after trimming
+  if (text === '') {
+    return '';
+  }
+
+  // Loop through each character in the string
+  for (let i = 0; i < text.length; i++) {
+    // Check if the current character is not a space
+    if (text[i] !== ' ') {
+      // Return the first non-space character
+      return text[i];
+    }
+  }
+
+  // If all characters are spaces, return an empty string
+  return '';
 }
 
 // Do not change the code below
